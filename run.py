f08ac17394fbf21cb7059a40b268b6d804538aa9
dallascard <dcard@andrew.cmu.edu>
2018-02-07 20:39:41 -0800

    debug

--- run.py
+++ run.py
@@ -124,7 +124,6 @@ def print_docs(model, docs, n=20):
         if doc_i < n:
             doc = doc.astype('float32')
             z=model.topic_prop(doc)
-            print(z)
             theta = np.exp(z) / np.sum(np.exp(z))
             print(theta)
 
